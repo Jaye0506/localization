ae2b33692826c0251a088fbe49abed3cc0da5343
linzy <linzy@qq.com>
2024-01-08 16:00:37 +0800

    update

--- pages/Font_Viewer.py
+++ pages/Font_Viewer.py
@@ -6,7 +6,7 @@ texts = st.text_input('输入需要渲染的文字')
 
 if texts:
     if upload_file:
-        imageFont = ImageFont.truetype(r'C:\Users\liyz3\AppData\Local\Microsoft\Windows\Fonts\simhei.ttf', 64)
+        imageFont = ImageFont.truetype(upload_file, 64)
         image = Image.new(mode='L', size=(1080,128), color=224)
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), texts, font=imageFont)
